ae60883044d162e126449cf01d0b8e657149a1bd
Instructor Victor <vcampos@swccd.edu>
2024-05-06 22:37:06 -0700

    Cleaned up code
    Removed stray code, cleaned up algorithms.

--- maincode.js
+++ maincode.js
@@ -1,18 +1,16 @@
 // Auto-run code regarding detecting an account (Remember Me)
-// So, not in a Function
-// Check localStorage for what's in "Remember Me"
 let uid = localStorage.getItem("rememberMe");
 console.log("Last login was: " + uid);
 
 // Create null DB object <<<<
 let myDB;
 
-// Create empty Current Comic
+// Create empty Current Comic for editing
 let comicCurrent = "";
 
-// Based on UID, move the view (User) to the right screen
+// Based on UID, move the view (User) to the correct screen
 if(uid === null || uid === undefined || uid === false || uid === "") {
-    // There was no user before, so send them to WElcome
+    // There was no user before, so send them to Welcome
     loadPage("welcome.html");
 } else {
     // Load the profile data from localstorage, .parse() it to use it
@@ -45,119 +43,89 @@ if(uid === null || uid === undefined || uid === false || uid === "") {
 // Detect when Onsen UI is read to be used
 ons.ready(function() {
     console.log("Onsen UI is ready!");
-});
+}); // END Onsen detection
 
-// One possible way to detect a device,
-// so that certain runs (or not) per device
+// Detecting an iPhone via ons Object
 if (ons.platform.isIPhoneX()) {
+    console.log("Detected iPhone");
     document.documentElement.setAttribute('onsflag-iphonex-portrait', '');
     document.documentElement.setAttribute('onsflag-iphonex-landscape', '');
-}
+} // END iPhone detection
+
+// Event Listener for running device-only code
+function onDeviceReady() {
+    console.log("The Device is: " + device.platform);
+} // END onDeviceReady()
+document.addEventListener("deviceready", onDeviceReady, false);
 
 console.log("App is ready");
 
-// Function to load a page, based on an ID
-// This type of code (a function) only runs when something makes it run
+// Function to load a screen, based on an ID
 function loadPage(pageID) {
     console.log("loadPage() is running");
     console.log("about to load: " + pageID);
-    // Using the Onsen UI Stack ("appNav") load/display a new page/screen
-    // .getElementById() is plain old JS POJS
-    // .bringPageTop() is Onsen-specific code: 
     document.getElementById("appNav").bringPageTop(pageID);
 } // END loadPage()
-
-// For testing purposes: add a "hard coded" landing page
-// So, MUST be removed, once we create "Remember Me" code 
-// zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
-// loadPage("welcome.html");
 
 // Function to make the Side Menu open
 function sideMenuOpen(menuID) {
     console.log("sideMenuOpen() is running");
     console.log("about to open side menu of: " + menuID);
-    // Now open the menu in question
     document.getElementById(menuID).open();
 } // END sideMenuOpen() 
 
-// Function to set up navigation via side menu
+// Function to load a screen via side menu
 function loadPageViaMenu(pageID) {
     console.log("loadPageViaMenu() is running");
     console.log("about to load this page: " + pageID);
     // Close the menu before moving to a new screen
-    // To-do: make it smarter to know WHICH menu to close
     document.getElementById("menuCust").close();
-    // And then load the page in question ORIGINAL 
-    // document.getElementById("appNav").bringPageTop(pageID);
-            // with promise
-        document.getElementById("appNav").bringPageTop(pageID).then(function (result) {
-                console.log("Loaded: " + result.id);
-                if(result.id === "homeCustCollection") { 
-                    console.log("Moved to Collection screen");
-                    // Show table of comics
-                    comicTable(); // Table clickability is in comicTable()
-                } else { 
-                    console.log("Moved to a non-Collection screen");
-                }; // END If..Else detect Collection screen
-            }).catch(function (err){console.log("Error: " + err)}); // END .bringPageTop() with JS Promise
+    document.getElementById("appNav").bringPageTop(pageID).then(function (result) {
+        console.log("Loaded: " + result.id);
+        if(result.id === "homeCustCollection") { 
+            console.log("Moved to Collection screen");
+            // Show table of comics
+            comicTable(); // Table clickability is in comicTable()
+        } else { 
+            console.log("Moved to a non-Collection screen");
+        }; // END If..Else detect Collection screen
+    }).catch(function (err){console.log("Error: " + err)}); // END .bringPageTop() with JS Promise
 } // END loadPageViaMenu() {
 
 // Function to log out
 function logOut() {
     console.log("logOut() is running");
-    // To-do list for this Fn:
-    // Confirm log out? Save any unsaved data/products/history/etc
-    // Play a sound? Vibrate? 
-
-    // Using the SWITCH (conditional statement) to confirm a logout...
-    // Made from swith() and each possiblity is a case (with colon)
-    //  that ends with break;
-    // Basic JS Confirm box is too basic
-    // window.confirm();
     // Using the Onsen UI Confrim box, to tap into the switch()
     ons.notification.confirm("Are you sure you want to log out?").then(function (response){ 
-                    switch(response){
-                        case 0:
-                            console.log("do not want to log out");
-                            // To-do: navigator.vibrate(1000); // remember to activate plugin
-                        break;
-
-                        case 1:
-                            console.log("YES log out");
-                            document.getElementById("menuCust").close();
-                            document.getElementById("menuAdmin").close();
-                            document.getElementById("menuKids").close();
-                            // Clear HISTORY! va .resetToPage (instead of bringPageTop())
-                            document.getElementById("appNav").resetToPage("welcome.html");
-                            // Set log out data to "Remember Me" - REMOVE it
-                            localStorage.removeItem("rememberMe");
-                            // Alternative, "empty" it
-                            // localStorage.setItem("rememberMe", "");
-                            // .setItem() creates or replaces data @ a mem location
-                        break;
-
-                        case "Maybe":
-                            console.log("Maybe, but save, nonethless");
-                        break;
-
-                        default:
-                        // For an unknown possiblity
-                            console.log("??????? IDK!");
-                        break;
-                    } // END swith()
-
+        switch(response){
+            case 0:
+                console.log("do not want to log out");
+                // To-do: navigator.vibrate(1000); // remember to activate plugin
+            break;
+            case 1:
+                console.log("YES log out");
+                document.getElementById("menuCust").close();
+                document.getElementById("menuAdmin").close();
+                document.getElementById("menuKids").close();
+                document.getElementById("appNav").resetToPage("welcome.html");
+                // Set log out data to "Remember Me" - REMOVE it
+                localStorage.removeItem("rememberMe");
+            break;
+            case "Maybe":
+                console.log("Maybe, but save, nonethless");
+            break;
+            default:
+                console.log("??????? IDK!");
+            break;
+        } // END swith()
     }); // END of .confirm().then()
 } // END logOut()
 
-// Fuctnion for SignU p
+// Function for signing up for an account
 function signUp() {
     console.log("signUp() is running");
 
     // Create a pattern for a strong password that requires
-    // [a-z] any characters form a - z LC
-    // [A-Z]any characters form A - Z UC
-    // [0-9] any numbers
-    // [!@#%:] 
     const strongPWD = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\!\@\#\$\^\*])(?=.{7,})");
 
     // Create variables to read what was in the fields
@@ -166,7 +134,7 @@ function signUp() {
     let valSignUpPWDConf = document.querySelector("#signupPWDConf").value;
     // Create a var to check if they are child or not; default, yes a child
     let valSignUpAge = false;
-    // Next read the fields and make a decisiion
+    // Next read the fields and make a decision 
     if(document.querySelector("#signUpAgeTrue").checked) {
         console.log("Yes, at least 13");
         valSignUpAge = true;
@@ -174,14 +142,13 @@ function signUp() {
         console.log("No, under 13");
         valSignUpAge = false;
     } // END If..Else checking Age of 13+
-
     console.log(valSignUpEmail, valSignUpPWD, valSignUpPWDConf, valSignUpAge);
 
-    // First check for strong password
+    // First, check for strong password
     if(strongPWD.test(valSignUpPWD)){ 
         console.log("TRUE! Password is strong");
 
-        // Next check all fields are completed
+        // Next check all fields are filled in
         if(valSignUpEmail === "" || valSignUpPWD === "" || valSignUpPWDConf === "") { 
             ons.notification.alert("Please fill all fields!");
         } else {
@@ -191,9 +158,9 @@ function signUp() {
                 document.getElementById("signupPWD").value = "";
                 document.getElementById("signupPWDConf").value = "";
             } else { 
-               // Next check if account already exists or not in localStorage
-               // But first lowercase-ify the email for usability
-               let tmpValSignUpEmail = valSignUpEmail.toLowerCase();
+                // Next check if account already exists or not in localStorage
+                // But first lowercase-ify the email for usability
+                let tmpValSignUpEmail = valSignUpEmail.toLowerCase();
                 if(localStorage.getItem(tmpValSignUpEmail) === null) { 
                     console.log("New account to create: " + tmpValSignUpEmail);
                     // Before we save the profile data, bundle it in JSON format
@@ -202,15 +169,13 @@ function signUp() {
                         "pwd" : valSignUpPWD,
                         "age" : valSignUpAge
                     };
-                     // Covert the complex Object into a plain String via JSON.stringify(tmpUser)
-                    // So set aside a memory loc for this user profile
-                    // save the complex data as a "simple" string
+                    // Convert the complex Object into a plain String via JSON.stringify(tmpUser)
                     localStorage.setItem(tmpUser._id, JSON.stringify(tmpUser));
                     ons.notification.alert("Account created!");
+                    // Clear fields and checkboxes
                     document.getElementById("signupEmail").value = "";
                     document.getElementById("signupPWD").value = "";
                     document.getElementById("signupPWDConf").value = "";
-                    // And clear checkboxes
                     document.getElementById("signUpAgeTrue").checked = false;
                     document.getElementById("signUpAgeFalse").checked = false;
                 } else {
@@ -257,72 +222,37 @@ function logIn() {
                     document.getElementById("appNav").resetToPage("homeAdmin.html");
                     break;
                 default:
-                    // Some possibility I can't even think of!
                     ons.notification.alert("IDK");
                     break;
             } // END Switch() for account type
 
             // Set up "Remembe Me" when you log in
             localStorage.setItem("rememberMe", tmpValLoginEmail); 
-            initDB(); // Init DB for user <<<<<<
+            initDB(); // Init DB for user that just logged in
         } else {
             ons.notification.alert("Passwords don't match!");
         } // END If.else pwd check
     } // END If..Else account check
 } // END logIn()
 
-/*
-    Notes on saving data:
-    Cache - temporary storage as your app runs; stores simple data; easy; not great for apps
-    Cookie - more-permanent; simple data; easy; not great for apps
-    localStorage - permament; more-complex data (with tricks); less-easy; great for apps
-                    ; profiles (harder); "remember me"; simple user account
-    DBs - permament; complex data; not-easy; best for apps; profiles
-        WebSQL; IndexDB; MonogoDB; MariaDB; PouchDB; CouchDB - NoSQL DB - No-infrastructure needed
-        (NOT MySQL, SQL, Oracle, FoxPro, ) - Classic DB - Infrastructure
-*/
 
-/*
-    Notes on Remember Me feature
-    #1 When the app starts, check who last logged in, so you can
-        send people to the right place at start
-    #2 When you log in, keep track of who logged in, so you can set a localStorage
-        data of who the last person was, which ties into #3 (also ties into #1)
-    #3 When you log out (switch accounts), keep track of who that is, so you can
-        delete (or clear) that bit of memory, which ties into #1
-*/
-
-/*
-// POUCHDB TESTING
-let db1 = new PouchDB("data1");
-let appNow = JSON.stringify(new Date());
-console.log("App time is: " + appNow);
-let tmpInfo1 = {
-    "_id" : appNow,
-    "note" : uid
-};
-
-db1.put(tmpInfo1).then(function (response) {
-  console.log("updated app: " + response.rev);
-}).catch(function (err) {
-  console.log("got an error:" + err);
-});
-*/
-
-// ------------------------- NEW
 // Function to initialize a PouchDB database
 function initDB() {
-console.log("initDB() is running");
-let emailForDB = localStorage.getItem("rememberMe");
-myDB = new PouchDB(emailForDB);
-myDB.info().then(function (result) {
-        console.log("DB Info -Name:", result.db_name, "-Docs: " + result.doc_count, "-Updates: " + result.update_seq);
-    }).catch(function (err) {
-        console.log(err);
-    });
-return myDB;
+    console.log("initDB() is running");
+    // Get the current user's email
+    let emailForDB = localStorage.getItem("rememberMe");
+    // Use the current user's email for DB name
+    myDB = new PouchDB(emailForDB);
+    // Show some info about the DB
+    myDB.info().then(function (result) {
+            console.log("DB Info -Name:", result.db_name, "-Docs: " + result.doc_count, "-Updates: " + result.update_seq);
+        }).catch(function (err) {
+            console.log(err);
+        }); // END .info()
+    return myDB;
 } // END initDB()
 
+// Prepare a comic for storage
 function comicPrep() {
     console.log("comicPrep() is running");
     // Get the values of the fields
@@ -333,7 +263,7 @@ function comicPrep() {
 
     console.log("Collecting data: ", valInputSaveTitle, valInputSaveNumber, valInputSaveYear, valInputSaveComment);
 
-    // Build the _id
+    // Build the _id, lowercaseify and strip out special characters
     let tmpTmpComic = valInputSaveTitle.replace(/\W/g, "").toLowerCase() + valInputSaveYear + valInputSaveNumber;
 
     // Bundle the data via JSON
@@ -346,12 +276,13 @@ function comicPrep() {
     }; // END JSON data
 
     // Check bundle
-    console.log("Bundled data: " + tmpComic);
+    console.log("Bundled data: " + Object.keys(tmpComic));
 
     // Return data to rest of app
     return tmpComic;
 } // END comicPrep()
 
+// Function to save a comic
 function comicSave() {
     console.log("comicSave() is running");
 
@@ -359,6 +290,7 @@ function comicSave() {
     let aComic = comicPrep();
     console.log("About to save comic: " + aComic._id);
 
+    // Save the comic to PouchDB
     myDB.put(aComic).then(function (result) {
         console.log("Saved comic: " + result.ok);
         ons.notification.alert("Comic Saved!");
@@ -377,6 +309,8 @@ function comicSave() {
 // Show table of comics
 function comicTable() {
     console.log("comicTable() is running");
+    
+    // Get all the comic data and alphabetize it
     myDB.allDocs({"ascending":true, "include_docs":true}).then(function (result){
         console.log("Getting comic data: " + result);
         // For first run, no data:
@@ -385,16 +319,17 @@ function comicTable() {
             // Set the header to their email
             let myEmail = localStorage.getItem("rememberMe");
             document.querySelector("#myCollection").innerHTML = myEmail;
-            // Add a message to empty <div>
+            // Add a message to placeholder <div>
             document.querySelector("#divComicTable").innerHTML = "No comics, yet. Save some!";
         } else {
             console.log("Some comics to display: " + result.rows.length);
             let myEmail = localStorage.getItem("rememberMe");
             document.querySelector("#myCollection").innerHTML = myEmail;
+            
             // Set up Variable to hold start of Table of comics
             let comicData = "<table> <tr> <th>Title</th> <th>#</th> </tr>";
 
-            // Conditional For loop to create X number of rows based on data .length property
+            // Conditional For loop to create i number of rows based on data .length property
             for(let i = 0; i < result.rows.length; i++) {
                 // Be very careful of opening closing "" and ''  and +=
                 // Embed a Class on each <tr> for clickability later
@@ -408,11 +343,8 @@ function comicTable() {
             // Complete the <table>
             comicData += "</table>";
 
-            // Display on-screen in empty <div>
+            // Display on-screen in placeholder <div>
             document.querySelector("#divComicTable").innerHTML = comicData;
-
-            // Set up <table> clickablity // NOT needed actually
-            // let clickComicTable = document.querySelector("#divComicTable");
 
             // Gather list of clickable Rows of comics
             let tableRows = document.querySelectorAll("tr.btnComicInfo");
@@ -422,9 +354,8 @@ function comicTable() {
                 // After click, run comicEditInfo(this) and pass on "this" row info
                 aRow.addEventListener("click", function () { comicEditInfo(this); }); 
             } // END FOR loop adding listeners
-
         } // END If..Else checking no comic data to display
-    }).catch( function (err){ console.log("Failure getting comic table data: " + err.message); }); // END .catch()
+    }).catch(function (err){ console.log("Failure getting comic table data: " + err.message); }); // END .catch()
 } // END comicTable()
 
 // Subroutine to delete entire DB
@@ -511,6 +442,7 @@ function comicEditInfoClose() {
 // Function to delete comic we are currently viewing; uses comicCurrent global variable
 function comicEditDelete() {
     console.log("About to delete: " + comicCurrent);
+    
     // First .get() the current comic .then() we .remove() it [note use of (comic)]
     myDB.get(comicCurrent).then(function (comic){
         ons.notification.confirm("Are you sure you want to delete this comic?").then(function (response){
@@ -520,7 +452,6 @@ function comicEditDelete() {
                     console.log("Canceled comic delete");
                 break;
                 case 1:
-                    //return myDB.remove(comic).then(function (result){  // Uses return from docs, but makes break unreachable
                     myDB.remove(comic).then(function (result){
                         console.log("Single comic deleted: " + result.ok);
                         // Redraw table
@@ -567,11 +498,3 @@ function comicEditUpdate() {
         });
     }).catch(function (err){console.log("Error updating comic:" + err);});
 } // END comicEditUpdae()
-
-
-// Event Listener for running device-only code
-document.addEventListener("deviceready", onDeviceReady, false);
-function onDeviceReady() {
-    console.log("!!!!!!!!!!!!!!The Device is: " + device.platform);
-
-} // END onDeviceReady()
